bd2e24cf65bb91c0179e42c4dfd977e6cfad2d3b
abdul qayyum <qayyumgb96@gmail.com>
2022-09-17 04:52:40 +0500

    static data

--- mail_handler.php
+++ mail_handler.php
@@ -1,9 +1,34 @@
 <?php
-$to = "somebody@example.com";
-$subject = "My subject";
-$txt = "Hello world!";
-$headers = "From: webmaster@example.com" . "\r\n" .
-"CC: somebodyelse@example.com";
+$name=$_POST['name'];
+$email=$_POST['email'];
+$msg=$_POST['message'];
+$to= "qayyumgb96@gmail.com"; // Receiver Email ID, Replace with your email ID
+$subject='New Query';
+// $message="Name :".$fname."\n"."Wrote the following :"."\n\n".$msg;
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+$headers .="From:".$email;
 
-mail($to,$subject,$txt,$headers);
+$message = '
+<html>
+<head>
+<title>HTML email</title>
+</head>
+<body>
+<p>This email contains HTML Tags!</p>
+<table>
+<tr>
+<th>Firstname</th>
+<th>Lastname</th>
+</tr>
+<tr>
+<td>John</td>
+<td>Doe</td>
+</tr>
+</table>
+</body>
+</html>
+';
+
+mail($to, $subject, $message, $headers);
 ?>
