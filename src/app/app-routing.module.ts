992556769fb9ddac7f61886d819f61f96c3b85ac
Muhammad Shahriyar <Shahriyar78622@outlook.com>
2025-04-15 15:29:09 +0500

    mobile changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { ExtraOptions, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', loadChildren: () => import('./main/main.module').then(m => m.MainModule) },
 ];
 
+const routerOptions: ExtraOptions = {
+  scrollPositionRestoration: 'enabled', // <- this scrolls to top automatically
+  anchorScrolling: 'enabled',
+};
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
     initialNavigation: 'enabledBlocking',
-    scrollPositionRestoration: 'enabled'
+    scrollPositionRestoration: 'enabled',
+    anchorScrolling: 'enabled',
 })],
   exports: [RouterModule]
 })
--- src/app/main/main-layout/main-layout.component.ts
+++ src/app/main/main-layout/main-layout.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./main-layout.component.scss']
 })
 export class MainLayoutComponent {
-
+  operSidePanel:boolean = false
 }
